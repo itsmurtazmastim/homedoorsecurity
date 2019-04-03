db2076f0be91bcd2f2ddd25fd668f29e126ed690
Murtaz <itsmurtaz@gmail.com>
2019-04-03 18:55:02 +0530

    Adding permissions for READ PHONE STATE

--- app/src/main/java/com/example/homedoorsecurity/MainActivity.java
+++ app/src/main/java/com/example/homedoorsecurity/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.homedoorsecurity;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Telephony;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.gsm.SmsManager;
 import android.util.Log;
@@ -18,7 +22,7 @@ import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     Button btnSendSMS;
     EditText txtPhoneNo;
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     boolean bRemoteDetailsRegistered = false;
     String strPhoneNumber;
     String strEndpointURL;
+
+    private static final int REQUEST_READ_PHONE_STATE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
         txtMessage = findViewById(R.id.editText2);
 
         registerReceiver(usbReceiver, new IntentFilter("usbStateChanged"));
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+        } else {
+            //TODO
+        }
+
         btnSendSMS.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
@@ -52,6 +66,14 @@ public class MainActivity extends AppCompatActivity {
                 txtMessage.setEnabled(false);
                 bRemoteDetailsRegistered = true;
                 Toast.makeText(MainActivity.this, " Remote Registration successful strPhoneNumber = " + strPhoneNumber + " strEndpointURL " + strEndpointURL, Toast.LENGTH_LONG).show();
+
+
+
+
+                LaunchBrowser();
+                String strRemoteEndpoint = strEndpointURL + "/remote.html";
+                String SMSText = getResources().getString(R.string.app_name) + " " + getResources().getString(R.string.door_ring_msg) + " " + strRemoteEndpoint;
+                sendSMS(strPhoneNumber, SMSText);
             }
                 // String strPackageName = getResources().getString(R.string.browser_package_name);
                 // KillApplication(strPackageName);
@@ -133,7 +155,6 @@ public class MainActivity extends AppCompatActivity {
     public void KillApplication(String KillPackage)
     {
         // ActivityManager am = (ActivityManager)this.getSystemService(Context.ACTIVITY_SERVICE);
-
         List<ActivityManager.RunningAppProcessInfo> processList;
         ActivityManager am;
 
@@ -148,13 +169,11 @@ public class MainActivity extends AppCompatActivity {
         // List<ActivityManager.RunningAppProcessInfo> processList = am.getRunningAppProcesses();
 
         Log.d("HomeDoorSecurity", " Size of processes is " + processList.size());
-
         for(int i = 0; i < processList.size(); ++i){
             Log.d("HomeDoorSecurity", processList.get(i).processName);
         }
 
         List<ActivityManager.RunningTaskInfo> runningTask = am.getRunningTasks(1000);
-
         Log.d("HomeDoorSecurity", " Size of Running Task is " + runningTask.size());
 
         for(int i = 0; i < runningTask.size(); ++i){
@@ -162,14 +181,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         List<ActivityManager.RunningServiceInfo> runningServices = am.getRunningServices(1000);
-
         Log.d("HomeDoorSecurity", " Size of Running Services is " + runningServices.size());
-
         for(int i = 0; i < runningServices.size(); ++i){
             Log.d("HomeDoorSecurity", runningServices.get(i).service.toString());
         }
 
         am.killBackgroundProcesses(KillPackage);
         Toast.makeText(getBaseContext(),"Process Killed : " + KillPackage  ,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STATE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //TODO
+                }
+                break;
+
+            default:
+                Log.d("HomeDoorSecurity", "onRequestPermissionsResult called for Request Code %d" + requestCode);
+                break;
+        }
     }
 }
